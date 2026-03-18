624cba994d1ef77b55970a33811657199a984512
Josie Ditmars <josieditmars.com>
2026-03-18 18:38:37 -0400

    fix timezone

--- src/main/java/com/compost/CompostReminderApplication.java
+++ src/main/java/com/compost/CompostReminderApplication.java
@@ -3,11 +3,13 @@ package com.compost;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class CompostReminderApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
         SpringApplication.run(CompostReminderApplication.class, args);
     }
 }
